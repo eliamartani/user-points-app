207fc68f48d0327c8967a0c281c847a66e974692
Eliamar Tani <eliamartani@gmail.com>
2021-03-14 22:02:24 +0000

    Update file organization

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = require('http').Server(app);
 const port = process.env.PORT || 3000;
 
 // Socket.io
-const io = require('socket.io')(server);
+const socketio = require('socket.io');
 
 // Next.js
 const next = require('next');
@@ -12,26 +12,31 @@ const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
 
-io.on('connection', socket => {
-  socket.on('message', ({ room, user, message }) => {
-    if (!room) {
-      throw new Error('[message] Room is missing');
-    }
-
-    socket.broadcast.emit(`message:${room}`, { message, user });
-  });
-
-  socket.on('clear', room => {
-    if (!room) {
-      throw new Error('[clear] Room is missing');
-    }
-
-    socket.broadcast.emit(`clear:${room}`);
-  });
-});
-
 nextApp.prepare().then(() => {
+  const io = new socketio.Server();
+
+  io.attach(server);
+
   app.get('*', (req, res) => nextHandler(req, res));
+
+  io.on('connection', socket => {
+    socket.on('message', ({ room, user, message }) => {
+      if (!room) {
+        throw new Error('[message] Room is missing');
+      }
+
+      socket.broadcast.emit(`message:${room}`, { message, user });
+    });
+
+    // Not imlpemented yet
+    socket.on('clear', room => {
+      if (!room) {
+        throw new Error('[clear] Room is missing');
+      }
+
+      socket.broadcast.emit(`clear:${room}`);
+    });
+  });
 
   server.listen(port, err => {
     if (err) throw err;
